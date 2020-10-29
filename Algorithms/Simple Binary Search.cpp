6b7ae20b62ed03e96b421c6ac019c5415faeb204
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2020-10-29 19:14:35 +0600

    Update Simple Binary Search.cpp

--- Algorithms/Simple Binary Search.cpp
+++ Algorithms/Simple Binary Search.cpp
@@ -11,39 +11,39 @@ using namespace std;
 
 int BinarySearch( int DATA[], int LB, int UB, int ITEM )
 {
-	int BEG = LB, END = UB, MID = ( int ) ( BEG + END ) / 2;
+    int BEG = LB, END = UB, MID = ( int ) ( BEG + END ) / 2;
 
-	while( ( BEG <= END ) && ( DATA[MID] != ITEM ) ) {
-		if( ITEM < DATA[MID] ) {
-			END = MID - 1;
-		} else {
-			BEG = MID + 1;
-		}
-		MID = ( int ) ( BEG + END ) / 2;
-	}
-	int LOC = -1;
-	if( DATA[MID] == ITEM ) {
-		LOC = MID;
-	}
+    while( ( BEG <= END ) && ( DATA[MID] != ITEM ) ) {
+        if( ITEM < DATA[MID] ) {
+            END = MID - 1;
+        } else {
+            BEG = MID + 1;
+        }
+        MID = ( int ) ( BEG + END ) / 2;
+    }
+    int LOC = -1;
+    if( DATA[MID] == ITEM ) {
+        LOC = MID;
+    }
 	
-	return LOC;
+    return LOC;
 }
 
 int main()
 {
-	int ara[] = { 1, 3, 4, 5, 5, 7, 8, 12, 18 };
-	int item = 3;
-	int lowerBound = 0;
-	int upperBound = 8;
-	int index = BinarySearch( ara, lowerBound, upperBound, item );
+    int ara[] = { 1, 3, 4, 5, 5, 7, 8, 12, 18 };
+    int item = 3;
+    int lowerBound = 0;
+    int upperBound = 8;
+    int index = BinarySearch( ara, lowerBound, upperBound, item );
 
-	if( index == -1 ) {
-		printf("Item Not Found\n");
-	} else {
-		printf("%d is found at index %d\n", item, index);
-	}
+    if( index == -1 ) {
+        printf("Item Not Found\n");
+    } else {
+        printf("%d is found at index %d\n", item, index);
+    }
 
-    return 0;
+     return 0;
 }
 
 //ALHAMDULILLAH

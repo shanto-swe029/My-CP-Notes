36b01eaaa616102b702c699de5124ff892409f41
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2020-10-31 14:18:43 +0600

    Update 208-Binary Search: Min In A Canyon.cpp

--- Algorithms/208-Binary Search: Min In A Canyon.cpp
+++ Algorithms/208-Binary Search: Min In A Canyon.cpp
@@ -6,6 +6,29 @@
 |____________________________________|
 */
 
+/*
+	Problem Statement:
+	---------------------------------------------------------------------------------
+	Suppose, you have an array. The first part of the array is non-increasing and 
+	the final part of the array is non-decreasing. You have to find the minimum
+	value of this array.
+	For example :
+	Let's consider an array like { 5, 4, 3, 2, 1, 3, 5, 7, 9 }.
+	The minimum number of this array = 1.
+	
+	For calrification : size of any of the two parts ( but not both ) can be zero.
+	
+	
+	Solution Approach:
+	---------------------------------------------------------------------------------
+	Running Binary Search is the most efficient way here. But what will be the ITEM
+	value here is the main query.
+	Well, in this case, ITEM value is a variable. We have to compare DATA[MID] with
+	DATA[MID - 1].
+	This is the key to solving this problem. I am throwing the rest understanding of
+	this approach on my code. I hope you will get it.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

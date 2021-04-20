a78c05ad3144c123a31e1f3b810fb387b9d164a7
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2021-04-20 23:48:21 +0600

    Update eGCD_Shanto.cpp

--- Algorithms/Euclidean Algorithm/eGCD_Shanto.cpp
+++ Algorithms/Euclidean Algorithm/eGCD_Shanto.cpp
@@ -9,10 +9,20 @@
     ----------------------------------------------------------
 */
 
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+// using pair
+typedef pair<int, int> pii;
+pii extendedEuclid(int a, int b) { // returns x, y | ax + by = gcd(a,b)
+  if(b == 0) return pii(1, 0);
+  else {
+    pii d = extendedEuclid(b, a % b);
+    return pii(d.second, d.first - d.second * (a / b));
+  }
+}
+
+// using pointer
 void bezoutCoefficient_Shanto(long long A, long long B, long long* x, long long* y, long long* gcd) {
     if(B == 0) {
         (*gcd) = A;
@@ -29,13 +39,21 @@ void bezoutCoefficient_Shanto(long long A, long long B, long long* x, long long*
 }
 
 int main( ) {
+    // using pointer
     long long A, B, x, y, gcd;
     long long *px, *py, *pgcd;
-    
     px = &x, py = &y, pgcd = &gcd;
     A = 12, B = 42;
-
     bezoutCoefficient_Shanto(A, B, px, py, pgcd);
     cout << A << " x (" << x << ") + " << B << " x (" << y << ") = " << gcd << endl;
+    
+    // using pair
+    pii coef;
+    coef = extendedEuclid(A, B);
+    long long gcd1 = __gcd(A, B);
+    long long x1 = coef.first;
+    long long y1 = coef.second;
+    cout << A << " x (" << x1 << ") + " << B << " x (" << y1 << ") = " << gcd1 << endl;
+    
     return 0;
 }

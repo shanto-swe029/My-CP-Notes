63c6b4d806ce12926a0706746fdccd5200624f99
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2021-05-23 11:54:02 +0600

    Update practice-01.cpp

--- Graph Theory/practice-01.cpp
+++ Graph Theory/practice-01.cpp
@@ -1,7 +1,11 @@
 /*
-			BISMILLAHIR-RAHMANIR-RAHIM
-					SWExSh4n.t0
-						SUST
+	What's here?
+	-------------------------------------------------------------------------------------
+	
+	From a directed graph - 
+	1. Create an adjacent list
+	2. For each node, find out how many nodes are connected with it along with the nodes.
+	3. Find out the indegree and outdegree for each node.
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,8 +15,8 @@ int main()
 	int numNodes, numEdges;
 	std::cin >> numNodes >> numEdges;
 
-	std::vector < std::vector < int > > v;
-	std::vector < std::vector < int > > u;
+	std::vector < std::vector < int > > v; // outdegree edges
+	std::vector < std::vector < int > > u; // indegree edges
 	std::vector < int > loop;
 	v.resize(numNodes);
 	u.resize(numNodes);
@@ -21,9 +25,9 @@ int main()
 	int n1, n2;
 	for( int i = 0; i < numEdges; i++ ) {
 		std::cin >> n1 >> n2;
-		v[n1 - 1].push_back(n2 - 1);
-		if( n1 != n2 ) u[n2 - 1].push_back(n1 - 1);
-		else loop[n1 - 1]++;
+		v[n1 - 1].push_back(n2 - 1); // saving for outdegree edges
+		if( n1 != n2 ) u[n2 - 1].push_back(n1 - 1); // saving for indegree edges
+		else loop[n1 - 1]++; // counting self-loop
 	}
 
 	// connected nodes list
@@ -34,7 +38,8 @@ int main()
 		for( int j = 0; j < u[i].size(); j++ ) 
 			std::cout << u[i][j] + 1 << ' ';
 		std::cout << endl;
-	} std::cout << endl;
+	}
+	std::cout << endl;
 
 	// indegree
 	for( int i = 0; i < numNodes; i++ ) {
@@ -44,7 +49,8 @@ int main()
 		for( int j = 0; j < loop[i]; j++ ) 
 			std::cout << i + 1 << ' ';
 		cout << endl;
-	} std::cout << endl;
+	}
+	std::cout << endl;
 
 	// outdegree
 	for( int i = 0; i < numNodes; i++ ) {
@@ -52,10 +58,11 @@ int main()
 		for( int j = 0; j < v[i].size(); j++ ) 
 			std::cout << v[i][j] + 1 << ' ';
 		cout << endl;
-	} std::cout << endl;
+	}
+	std::cout << endl;
+	
 	return 0;
 
-	// 
 }
 
 //	ALHAMDULILLAH
